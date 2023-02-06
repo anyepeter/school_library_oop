aa293b1a6a232cf435b8d7bb32e37b485af762ab
anyepeter <petzyrockchendi@gmail.com>
2023-02-06 23:02:47 +0100

    add the teacher class

--- student.rb
+++ student.rb
@@ -1,13 +1,12 @@
 require_relative './person'
 
-class Student < Person 
-    def initialize(classroom, age, name="Unknow", parent_permission: true)
-        super(id, age, name, parent_permission: parent_permission)
-        @classroom = classroom
-    end
+class Student < Person
+  def initialize(classroom, age, name = 'Unknow', parent_permission: true)
+    super(id, age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
-
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
