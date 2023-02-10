e483899827bf0f430eaaa49d939a0671c2a3475d
anyepeter <petzyrockchendi@gmail.com>
2023-02-10 06:56:48 +0100

    add the list person method

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require_relative 'book'
+require_relative 'person'
 
 class App
     def initialize
         @books = []
+        @people = []
 end
  
-def list_books
+def list_all_books
   @books.each do |book|
-    puts "Title: #{book.title} and Author: #{book.author}"
+    puts "Title: #{book.title}, Author: #{book.author}"
+end
+
+def list_all_people
+    @people.each do |person|
+        puts "[#{person.type}] name: #{people.name}, ID: #{person.id}, #{person.age}"
 end
 end
