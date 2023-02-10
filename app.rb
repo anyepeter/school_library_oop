693eacfa8ddfa219d0a152aab42f8398020e75bc
anyepeter <petzyrockchendi@gmail.com>
2023-02-10 07:44:10 +0100

    add the method to create a person

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require_relative 'book'
 require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
 
 class App
     def initialize
@@ -16,4 +18,43 @@ def list_all_people
     @people.each do |person|
         puts "[#{person.type}] name: #{people.name}, ID: #{person.id}, #{person.age}"
 end
+
+def create_person 
+    print "Do you want to create a student (1) or a teacher(2)? [Input the number]"
+    response = get.chomp
+    case response
+    when '1'
+        print "Age:"
+        age = get.chomp
+        print "name:"
+        name = get.chomp
+
+        print "Has parent permission? [Y/N]:"
+        permit = get.chomp
+        case permit
+        when 'Y','y'
+        student.Student.new(age, name, true)
+        @people.push(student)
+        puts "Person created successful"
+        when 'N','n' 
+        student.Student.new(age, name, false)
+        @people.push(student)
+        puts "Person created successful"
+        else 
+            puts "invalid"    
+        end
+    when '2'
+        print "Age:"
+        age = get.chomp
+        print "name:"
+        name = get.chomp
+        print "specialization:"
+        specialization = get.chomp
+        teacher = Teacher.new(specialization, age, name)
+        @people.push(teacher)
+        puts "Person created successful"
+    else
+        puts "Invalid"
+    end
+
 end
